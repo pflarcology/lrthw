c1dbf45d2b8d96bab18a56f43ff62dd55d134e03
Rory Grieve <rhrg500@gmail.com>
2016-10-18 12:41:37 +0000

    ex9

---
+++ ex9.rb
@@ -0,0 +1,19 @@
+# Here's some strange new stuff, remember type it exactly.
+
+# just a string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# \n means a new line each time
+months = "Jan\nFed\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+# use triple quotes to write whole section
+puts """
+There's something going on here. 
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+# don't forget to close it!
+"""
+
+# mistakes: make sure variables match
