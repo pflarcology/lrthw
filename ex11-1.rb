e35d1344221d6688fa3764491f51f51ff217ca5a
Rory Grieve <rhrg500@gmail.com>
2016-10-18 15:29:06 +0000

    ex11 and ex11-1

---
+++ ex11-1.rb
@@ -0,0 +1,6 @@
+print "What breed is your dog?"
+dog_breed = gets.chomp
+print "How old is your dog?"
+dog_age = gets.chomp
+
+puts "I see you have a #{dog_breed} and they are #{dog_age} years old."
