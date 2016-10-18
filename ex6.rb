c555b4ca0a72fabec8f6abe4605f9365bc437f2a
Rory Grieve <rhrg500@gmail.com>
2016-10-18 11:54:33 +0000

    ex6

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# assign integer variable
+types_of_people = 10
+# put variable in string
+x = "There are #{types_of_people} types of people."
+# new string variable
+binary = "binary"
+# new string variable
+do_not = "don't"
+# insert two variables into string
+y = "Those who know #{binary} and those who #{do_not}."
+
+# displays variable
+puts x
+# displays variable
+puts y
+
+# displays variable inside string
+puts "I said: #{x}."
+# displays variable inside string
+puts "I also said: '#{y}'."
+
+# variable equals boolean
+hilarious = false
+# string with boolean
+joke_evaluaton = "Isn't that joke so funny?! #{hilarious}"
+
+#displays string
+puts joke_evaluaton
+
+# new string variable
+w = "This is the left side of..."
+# new string variable
+e = "a string with the right side."
+
+# displays two string variables
+puts w + e
